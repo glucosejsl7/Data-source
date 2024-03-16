daa2bb0ea37d6cf500c49905dfd356bc78fe2a76
glucosejsl7 <122554576+glucosejsl7@users.noreply.github.com>
2024-03-16 20:58:14 +0700

    Create poster.js

---
+++ poster.js
@@ -0,0 +1,10 @@
+[
+  {"id" : "1", "uri" : "https://raw.githubusercontent.com/glucosejsl7/Data-source/main/search/Poster/poster1.jpg"},
+  {"id" : "1", "uri" : "https://raw.githubusercontent.com/glucosejsl7/Data-source/main/search/Poster/poster2.jpg"},
+  {"id" : "1", "uri" : "https://raw.githubusercontent.com/glucosejsl7/Data-source/main/search/Poster/poster3.jpg"},
+  {"id" : "1", "uri" : "https://raw.githubusercontent.com/glucosejsl7/Data-source/main/search/Poster/poster4.jpg"},
+  {"id" : "1", "uri" : "https://raw.githubusercontent.com/glucosejsl7/Data-source/main/search/Poster/poster5.jpg"},
+  {"id" : "1", "uri" : "https://raw.githubusercontent.com/glucosejsl7/Data-source/main/search/Poster/poster6.jpg"},
+  {"id" : "1", "uri" : "https://raw.githubusercontent.com/glucosejsl7/Data-source/main/search/Poster/poster7.jpg"}
+
+]
